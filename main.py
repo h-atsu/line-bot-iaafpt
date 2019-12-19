c71d9b666ce689ab6c1a098c2510c81a9819ef5c
h-atsuhiro <jhuim08@gmail.com>
2019-12-19 15:17:56 +0900

    reviewed

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 from flask import Flask, request, abort
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
 import os
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-
+import csv
 app = Flask(__name__)
 
-#環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
+
+
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
-@app.route("/")
-def hello_world():
-    return "hello world!"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -41,13 +31,56 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    phrase = event.message.text.split(" ")
+    csv_file = open("./iaafpt_m.csv", "r", encoding="utf_8",errors="",newline="")
+    f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+    if(phrase[0] == "HJ"):
+        for row in f:
+            if(row[0] == phrase[1]):
+                textmessage = row[1]
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(phrase[0] +":"+ phrase[1] + "m \nIAAF得点は" + textmessage + "ptです"))
+
+    if(phrase[0] == "PV"):
+        for row in f:
+            if(row[2] == phrase[1]):
+                textmessage = row[3]
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(phrase[0] +":"+ phrase[1] + "m \nIAAF得点は" + textmessage + "ptです"))
+
+    if(phrase[0] == "LJ"):
+        for row in f:
+            if(row[4] == phrase[1]):
+                textmessage = row[5]
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(phrase[0] +":"+ phrase[1] + "m \nIAAF得点は" + textmessage + "ptです"))
+
+    if(phrase[0] == "TJ"):
+        for row in f:
+            if(row[6] == phrase[1]):
+                textmessage = row[7]
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(phrase[0] +":"+ phrase[1] + "m \nIAAF得点は" + textmessage + "ptです"))
+
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage("跳躍種目のIAAFptを返します! \n{種目} {記録} \nの順で入力してください"))
+
+
+
+
+
+
 
 if __name__ == "__main__":
 #    app.run()
-    port = int(os.getenv("PORT"))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
